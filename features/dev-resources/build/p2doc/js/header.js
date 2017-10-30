6a57fd2c20f958c5dc9dbec15c2a5ec3b7b3abbe
Mohammed Nadeem Syed (MOSY) <MohammedNadeem.Syed@softwareag.com>
2017-10-30 17:00:52 +0530

    Removing unwanted files and folders

--- features/dev-resources/build/p2doc/js/header.js
+++ features/dev-resources/build/p2doc/js/header.js
@@ -38,3 +38,43 @@ function htmlDecode(input) {
     e.innerHTML = input;
     return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
 }
+var projectDescriptionElement;
+var showMoreElement;
+
+$(function() {
+    projectDescriptionElement = $('#project-description');
+    showMoreElement = $('.show-more');
+
+    projectDescriptionElement.hide();
+})
+
+function toggleHeader() {
+    projectDescriptionElement.stop().toggle('fast');
+    if(showMoreElement.text() === 'show more ...') {
+        showMoreElement.text('show less ...');
+    } else {
+        showMoreElement.text('show more ...');
+    }
+}
+
+function setDescription(content) {
+    projectDescriptionElement.html(htmlDecode(content));
+}
+
+function setOverview() {
+    var targetIuPage = "" + window.location.search;
+    if (targetIuPage == "" || targetIuPage == "undefined") {
+        return;
+    }
+
+    // Remove the leading '?'
+    targetIuPage = targetIuPage.substring(1);
+    
+    document.getElementById('iuOverview').src = targetIuPage;
+} 
+
+function htmlDecode(input) {
+    var e = document.createElement('div');
+    e.innerHTML = input;
+    return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+}
--- features/dev-resources/build/p2doc/js/toolbox.js
+++ features/dev-resources/build/p2doc/js/toolbox.js
@@ -13,4 +13,19 @@ $(function() {
             //$.cookie('toolbox-expanded', true);
         }
     );*/
+})$(function() {
+    var toolboxSelector = $('.toolbox');
+/*
+    toolboxSelector.css({top: -60});
+
+    $('#toggle-toolbox').toggle(
+        function () {
+            toolboxSelector.animate({"top": "0px"}, "fast");
+            //$.cookie('toolbox-expanded', true);
+        },
+        function () {
+            toolboxSelector.animate({"top": "-60px"}, "fast");
+            //$.cookie('toolbox-expanded', true);
+        }
+    );*/
 })
